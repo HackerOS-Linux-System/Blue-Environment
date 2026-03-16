92f9d4793a22972c107f2c47dd50cda50e2c8a8c
michal92299 <chudybyczek2137@gmail.com>
2026-03-16 19:41:23 +0100

    Delete src directory

--- src/bin/dock.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    tracing_subscriber::fmt()
-    .with_env_filter(tracing_subscriber::EnvFilter::from_default_env()
-    .add_directive("blue_environment=info".parse().unwrap()))
-    .init();
-    blue_environment::dock::run();
-}
