455222ab3a46c9f67ad08d4dfc1c540a23d8dbd5
michal92299 <chudybyczek2137@gmail.com>
2026-06-16 16:48:35 +0200

    Delete build.rs

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // Kompiluj jeden plik który importuje wszystkie - to generuje jeden slint_generated.rs
-    // z WSZYSTKIMI komponentami dostępnymi przez include_modules!()
-    slint_build::compile("ui/all.slint").expect("ui/all.slint");
-}
