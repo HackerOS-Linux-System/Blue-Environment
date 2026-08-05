27761337aff47c2b75eb8655d88de2e538d7b30e
michal92299 <chudybyczek2137@gmail.com>
2026-08-05 21:30:51 +0200

    Delete compositor directory

--- compositor/src/protocols/cursor_shape.rs
+++
@@ -1,7 +0,0 @@
-use smithay::delegate_cursor_shape;
-use smithay::wayland::tablet_manager::TabletSeatHandler;
-use crate::state::BlueState;
-
-impl TabletSeatHandler for BlueState {}
-
-delegate_cursor_shape!(BlueState);
