7cb934f9d8d6810addca901fce73679a04bdebcd
michal92299 <chudybyczek2137@gmail.com>
2026-08-12 12:32:08 +0200

    Update types.ts

--- src/lib/components/apps/Settings-App/types.ts
+++ src/lib/components/apps/Settings-App/types.ts
@@ -2,4 +2,4 @@ export type SettingsTab =
   | 'display' | 'personalization' | 'network' | 'wifi' | 'bluetooth' | 'cellular'
   | 'power' | 'panel' | 'language' | 'nightLight' | 'weather'
   | 'apps' | 'default_apps' | 'monitors' | 'printers' | 'users'
-  | 'accounts' | 'about' | 'security';
+  | 'accounts' | 'about' | 'security' | 'parental_controls';
