299b88f8aed0f502df0053a3fef7287d9d4c4aea
michal92299 <chudybyczek2137@gmail.com>
2026-09-01 14:11:35 +0200

    Create BlueVirtAppLazy.svelte

---
+++ src/lib/components/apps/Blue-Virt/BlueVirtAppLazy.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let component: any = null;
+  let failed = false;
+
+  import('./BlueVirtApp.svelte')
+    .then((mod) => (component = mod.default))
+    .catch(() => (failed = true));
+</script>
+
+{#if failed}
+  <div class="flex items-center justify-center h-full text-red-400 text-sm">Failed to load Blue Virt.</div>
+{:else if component}
+  <svelte:component this={component} {...$$restProps} />
+{:else}
+  <div class="flex items-center justify-center h-full bg-slate-950">
+    <div class="w-6 h-6 border-2 border-blue-500 border-t-transparent rounded-full animate-spin" />
+  </div>
+{/if}
